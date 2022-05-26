ee27c35f6f891629d1cc594cbbdbb11c56051049
devolonter <devolonter@gmail.com>
2022-05-26 22:44:04 +0200

    Update main page markup

--- lib/widgets/cconverter.dart
+++ lib/widgets/cconverter.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
+import 'package:cconverter/common/convert_pipe.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'calc_stack.dart';
 import 'numpad.dart';
 
 class MainPage extends StatelessWidget {
@@ -10,21 +11,43 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const bgColor = Color(0xFF2B2B2B);
+    const Color bgColor = Color(0xFF2B2B2B);
 
     return Theme(
-        data: ThemeData.from(
-          colorScheme: const ColorScheme.dark(),
-        ),
-        child: SafeArea(
-            child: Container(
-          color: bgColor,
-          child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: const [NumPad()],
-              )),
-        )));
+      data: ThemeData.from(
+        colorScheme: const ColorScheme.dark(),
+      ),
+      child: SafeArea(
+        child: Container(
+            color: bgColor,
+            child: Column(
+              children: [
+                Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: const [
+                          Expanded(
+                              child: CalcStack()
+                          )
+                        ],
+                      ),
+                    )
+                ),
+                Container(
+                    padding: const EdgeInsets.all(16),
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF3A3A3A),
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(16))),
+                    child: Center(child: NumPad(
+                      controller: ConvertPipe().numPadController,
+                    ))),
+              ],
+            )),
+      ),
+    );
   }
 }
 
