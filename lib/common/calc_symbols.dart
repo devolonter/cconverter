556a2b90f000d7dcca1a3480454b3581cd9814b5
devolonter <devolonter@gmail.com>
2022-05-27 19:14:15 +0200

    Improved calc symbols

--- lib/common/calc_symbols.dart
+++ lib/common/calc_symbols.dart
@@ -9,18 +9,47 @@ class CalcSymbol {
   }
 }
 
+class CalcSymbolAC extends CalcSymbol {
+  CalcSymbolAC._init() : super('AC');
+  static final CalcSymbolAC _instance = CalcSymbolAC._init();
+
+  factory CalcSymbolAC() {
+    return _instance;
+  }
+}
+
 class MathSymbolPlus extends CalcSymbol {
-  MathSymbolPlus() : super('+');
+  MathSymbolPlus._init() : super('+');
+  static final MathSymbolPlus _instance = MathSymbolPlus._init();
+
+  factory MathSymbolPlus() {
+    return _instance;
+  }
 }
 
 class MathSymbolMinus extends CalcSymbol {
-  MathSymbolMinus() : super('−');
+  MathSymbolMinus._init() : super('−');
+  static final MathSymbolMinus _instance = MathSymbolMinus._init();
+
+  factory MathSymbolMinus() {
+    return _instance;
+  }
 }
 
 class MathSymbolMul extends CalcSymbol {
-  MathSymbolMul() : super('×');
+  MathSymbolMul._init() : super('×');
+  static final MathSymbolMul _instance = MathSymbolMul._init();
+
+  factory MathSymbolMul() {
+    return _instance;
+  }
 }
 
 class MathSymbolDiv extends CalcSymbol {
-  MathSymbolDiv() : super('÷');
+  MathSymbolDiv._init() : super('÷');
+  static final MathSymbolDiv _instance = MathSymbolDiv._init();
+
+  factory MathSymbolDiv() {
+    return _instance;
+  }
 }
