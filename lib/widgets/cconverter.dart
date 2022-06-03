c869a30167e4d988493077b879efacfec0823499
devolonter <devolonter@gmail.com>
2022-06-03 13:43:10 +0200

    Removed test and resolved warrnings

--- lib/widgets/cconverter.dart
+++ lib/widgets/cconverter.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
